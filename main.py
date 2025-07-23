33f6ebf4a3580fdc5c7c137efc9d01c70c337369
brozoke <bzoschke@gmail.com>
2025-07-23 13:11:20 -0600

    draw player to screen, add hitbox for player

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame 
+from player import *
 from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
@@ -13,7 +18,9 @@ def main():
                 return
 
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
